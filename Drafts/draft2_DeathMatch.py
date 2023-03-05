46282a3aa780f5512c219ab34b3fc98ac4b11369
THOMAS JEON <hjeon21@my.bcit.ca>
2023-03-04 17:42:21 -0800

    Add draft for Sumo; kind of works without shield

---
+++ Drafts/draft2_DeathMatch.py
@@ -0,0 +1,44 @@
+from mindstorms import MSHub, Motor, MotorPair, ColorSensor, DistanceSensor, App
+from mindstorms.control import wait_for_seconds, wait_until, Timer
+from mindstorms.operator import greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to, equal_to, not_equal_to
+import math
+
+
+# Create your objects here.
+hub = MSHub()
+
+# Write your program here.
+hub.speaker.beep()
+motor_pair = MotorPair('A', 'B')
+color_sensor = ColorSensor('E')
+distance_sensor = DistanceSensor('D')
+hub = hub.motion_sensor
+
+motor_pair.set_default_speed(25)
+
+
+"""
+How this works:
+Assuming the border is a box or a circle, the robot will stay in one spot and continuously spins in place
+until it can find an object near it and rams into it.If the color sensor detects the border (assuming
+black tape is used), then it reverses.
+Continues to do this forever.
+"""
+
+
+def sumo():
+
+    while True:
+        distance = distance_sensor.get_distance_cm()
+        if color_sensor.get_color() == 'black':
+            motor_pair.move_tank(10, 'cm', 50, 50)
+        elif distance == None:
+            distance_sensor.light_up_all(50)
+            motor_pair.start(30)
+        elif distance <= 10:
+            distance_sensor.light_up_all(50)
+            motor_pair.move(100)
+        else:
+            motor_pair.start_tank(20, -20)
+
+sumo()
