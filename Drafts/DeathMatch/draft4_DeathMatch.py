064990f40ba9bf0632b26c19d96ee13d5eb011b9
THOMAS JEON <hjeon21@my.bcit.ca>
2023-03-07 22:41:45 -0800

    Adjust fourth draft for DeathMatch.  Might work without shield.

--- Drafts/DeathMatch/draft4_DeathMatch.py
+++ Drafts/DeathMatch/draft4_DeathMatch.py
@@ -2,6 +2,7 @@ from mindstorms import MSHub, Motor, MotorPair, ColorSensor, DistanceSensor, App
 from mindstorms.control import wait_for_seconds, wait_until, Timer
 from mindstorms.operator import greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to, equal_to, not_equal_to
 import math
+
 # Create your objects here.
 hub = MSHub()
 # Write your program here.
@@ -11,20 +12,40 @@ color_sensor = ColorSensor('E')
 distance_sensor = DistanceSensor('D')
 hub_degrees = hub.motion_sensor
 motor_pair.set_default_speed(25)
+
+"""
+Robot scans arena to see if there are any objects in the arena.
+
+If there are, then the robot runs towards it.  
+There isn't, then it spins around. 
+When it detects the border, it reverses. 
+"""
+
+
 def sumo():
     while True:
+    # forever loop
         distance = distance_sensor.get_distance_cm()
+        # initializes distance
         if color_sensor.get_color() == 'black':
             motor_pair.start(0, -50)
             wait_for_seconds(0.5)
+            # if robot detects border, then reverse for 0.5 seconds
         elif distance == None:
             distance_sensor.light_up_all(50)
             motor_pair.start(0, 50)
+            # if distance is none, then ram into robot
+            # this is necessary or else Mindstorms will give an error
         elif distance <= 15:
             distance_sensor.light_up_all(50)
             motor_pair.start(0, 50)
+            # if distance is 15 or under, then robot rams into other robot
         else:
-            motor_pair.start(20, 0)
+            motor_pair.start(30, -30)
+            # if there are no objects in the distance, spin around
+
 sumo()
+
+
 def main():
     print(sumo)
