5b50084c0c869af46b70d8fb5e6241a16aa31c72
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-03-08 16:24:36 -0800

    Added deathmatch5 and sanda

---
+++ Drafts/DeathMatch/draft5_DeathMatch.py
@@ -0,0 +1,53 @@
+from mindstorms import MSHub, Motor, MotorPair, ColorSensor, DistanceSensor, App
+from mindstorms.control import wait_for_seconds, wait_until, Timer
+from mindstorms.operator import greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to, equal_to, not_equal_to
+import math
+
+# Create your objects here.
+hub = MSHub()
+# Write your program here.
+hub.speaker.beep()
+motor_pair = MotorPair('A', 'B')
+color_sensor = ColorSensor('E')
+distance_sensor = DistanceSensor('F')
+hub_degrees = hub.motion_sensor
+motor_pair.set_default_speed(100)
+timer = Timer()
+
+"""
+Robot scans arena to see if there are any objects in the arena.
+
+If there are, then the robot runs towards it.
+When there isn't, it spins around.
+When it detects the border, it reverses.
+"""
+
+
+def sumo():
+    motor_pair.start_tank(50, -50)
+    while True:
+    # forever loop
+        distance = distance_sensor.get_distance_cm()
+        # initializes distance
+        if color_sensor.get_color() != 'green':
+            motor_pair.move(25, 'cm', 0, -100)
+            # if robot detects border, then reverse for 0.5 seconds and stops
+        elif distance == None or distance >= 15:
+            motor_pair.start_tank(50, -50)
+            # if distance is none, then ram into robot
+            # this is necessary or else Mindstorms will give an error
+        elif distance <= 6:
+            distance_sensor.light_up_all(50)
+            motor_pair.start(0, 100)
+            # if distance is 15cm or under, then robot rams into other robot
+        if timer.now() >= 10:
+            while True:
+                if color_sensor.get_color() != 'green':
+                    motor_pair.move(25, 'cm', 0, -100)
+                    timer.reset()
+                    break
+                else:
+                    motor_pair.start()
+                
+
+sumo()
---
+++ Drafts/FindBall/FindBall_slow_and_accurate.py
@@ -0,0 +1,51 @@
+from mindstorms import MSHub, Motor, MotorPair, ColorSensor, DistanceSensor
+from mindstorms.control import wait_for_seconds, Timer
+# Create your objects here.
+
+hub = MSHub()
+motor_pair = MotorPair('A', 'B')
+motor_c = Motor('C')
+c_sensor = ColorSensor('E')
+d_sensor = DistanceSensor('F')
+timer = Timer()
+
+def main():
+    hub.speaker.beep()
+    motor_pair.set_default_speed(20)
+    Scan()
+
+def Scan():
+    hub.motion_sensor.reset_yaw_angle()
+    timer.reset()
+    while True:
+        distance = d_sensor.get_distance_cm()
+        if c_sensor.get_color() == 'white' or c_sensor.get_color() == 'black':
+            motor_pair.stop()
+            motor_pair.move(timer.now(), 'seconds', 0, -20)
+            Rotate()
+            timer.reset()
+        else:
+            motor_pair.start()
+        if distance is not None and distance <= 8:
+            final_time = timer.now()
+            LiftArm()
+            motor_pair.move(final_time, 'seconds', 0, -20)
+            break
+    Finish()
+
+def Rotate():
+    hub.motion_sensor.reset_yaw_angle()
+    while hub.motion_sensor.get_yaw_angle() < 20:
+        motor_pair.start_tank(10, -10)
+    motor_pair.stop()
+
+def LiftArm():
+    motor_pair.stop()
+    wait_for_seconds(2)
+    motor_c.run_for_degrees(75, 20)
+
+def Finish():
+    motor_pair.stop()
+    hub.speaker.play_sound("Ha Ha Ha")
+    
+main()
